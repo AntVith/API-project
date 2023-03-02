43cbfb3d76a984f3c6fdd3f9bac57642be55b24f
Anthony <avithayathil23@gmail.com>
2023-03-02 15:39:29 -0500

    styled trips page

--- frontend/src/components/Bookings/index.js
+++ frontend/src/components/Bookings/index.js
@@ -12,16 +12,51 @@ const UserBookings = () =>{
 
     const bookingsObj = useSelector(state => state.bookings.bookings)
     const bookings = Object.values(bookingsObj)
-    console.log('book', bookings)
-    let starts = []
-
-    let ends = []
-
 
     useEffect(() => {
         // console.log('use')
         dispatch(getBookingsThunk())
     }, [dispatch])
+
+
+    const pastBookings = bookings.filter(booking => {
+        const splitEnd = booking.endDate.split('-')
+        const endYear = Number(splitEnd[0])
+        const endMonth = Number(splitEnd[1])
+        const endDay = Number(splitEnd[2])
+
+        const date = new Date()
+        const year = date.getFullYear()
+        const month = date.getMonth() + 1
+        const day = date.getDate()
+
+        if(endYear < year) return true
+        else if( endYear === year && endMonth < month) return true
+        else if(endMonth === month && endDay < day) return true
+        else{
+            return false
+        }
+    })
+    const futureBookings = bookings.filter(booking => {
+        const splitStart = booking.startDate.split('-')
+
+        const startYear = Number(splitStart[0])
+        const startMonth = Number(splitStart[1])
+        const startDay = Number(splitStart[2])
+
+        const date = new Date()
+        const year = date.getFullYear()
+        const month = date.getMonth() + 1
+        const day = date.getDate()
+
+        return startYear >= year && startMonth >= month && startDay >=day
+    })
+    console.log('book', bookings)
+    console.log('p',pastBookings)
+    console.log('f', futureBookings)
+
+
+
     const handleDeletion = async(id) => {
         const response = dispatch(deleteBookingThunk(id))
 
@@ -31,24 +66,15 @@ const UserBookings = () =>{
             alert('Canceled Successfully!')
         }
     }
-    // if(bookings.length){
-    //     const startsData = []
-    //      bookings.forEach(booking => {
-    //         starts.push(booking.startDate)
-    //     })
-    //     // if(starts != startsData) setStarts(startsData)
-    //     const endsData = []
-    //      bookings.forEach(booking => {
-    //         ends.push(booking.startDate)
-    //     })
-    //     // if(ends != endsData)setEnds(endsData)
-    // }
 
 
     return (
         <div id='bookings-container'>
+            <div id='bookings-layout' >
+            <div className='booking-filter-label'>Current/Future Bookings </div>
             <div id='all-bookings'>
-            {bookings.map(booking => (
+
+            {futureBookings.map(booking => (
 
                 <div className='bookingCard'>
                     <img
@@ -78,6 +104,33 @@ const UserBookings = () =>{
                     <button onClick={() => handleDeletion(booking.id)} id='cancel-booking-button'>Cancel Reservation</button>
                 </div>
                 ))}
+
+
+            </div>
+            <div className='booking-filter-label'>Previous Bookings </div>
+            <div id='all-bookings'>
+                {pastBookings.map(booking => (
+
+                <div className='bookingCard'>
+                    <img
+                    className='image-booking'
+                    // id={`image${spot.id}`}
+                    src={booking.Spot.previewImage}></img>
+                    <div id='topLineSpotCard'>
+                        <div>{booking.Spot.city}, {booking.Spot.state}</div>
+                    </div>
+                    <div id='start-date-row'>
+                        <div className='date-label'>Start Date </div>
+                        <div className='date-info'>{booking.startDate}</div>
+                    </div>
+                    <div id='end-date-row'>
+                        <div className='date-label'>End Date </div>
+                        <div className='date-info'>{booking.endDate}</div>
+                    </div>
+
+                </div>
+                ))}
+                </div>
             </div>
 
         </div>
